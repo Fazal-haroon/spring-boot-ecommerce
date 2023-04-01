123b94d5a8ce5e5aa07a033baa2b7f1bdc86e440
Fazal Haroon <kharoon187@gmail.com>
2023-04-01 15:33:26 +0400

    logger added for payment amount

--- src/main/java/com/example/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/example/ecommerce/controller/CheckoutController.java
@@ -11,10 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 //@CrossOrigin()
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     private final CheckoutService checkoutService;
 
@@ -37,6 +41,7 @@ public class CheckoutController {
 
     @PostMapping("/payment-intent")
     public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+        logger.info("paymentInfo.amount: " + paymentInfo.getAmount());
         PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
         String paymentStr = paymentIntent.toJson();
         return new ResponseEntity<>(paymentStr, HttpStatus.OK);
